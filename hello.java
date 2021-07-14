d348c92252a4698f8c9e6269bf8f5501073d9f33
udhaydevops <udhayafilesbackup@gmail.com>
2021-07-14 13:58:59 +0000

    child commit

--- hello.java
+++ hello.java
@@ -2,7 +2,11 @@ class Hello
 {
 public static void main(String args[])
 {
+<<<<<<< HEAD
 System.out.println("hello");
+=======
+System.out.println("hello -second commit");
+>>>>>>> 4377e27d45c37213c59852c6bb5cb02f08c597f3
 }
 }
 
